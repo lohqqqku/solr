a15a5b35b20cf5fafe64da294e7f48dc72d9d3a0
lohqqqku <xundianju1980@126.com>
2021-06-05 21:51:38 +0800

    整合ik中文分词和lucene的同义词测试

--- src/main/java/test/solrcloud/SolrCloudUtil.java
+++ src/main/java/test/solrcloud/SolrCloudUtil.java
@@ -1,8 +1,5 @@
 package test.solrcloud;
 
-import java.util.Enumeration;
-import java.util.Properties;
-
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
 
 public class SolrCloudUtil {
@@ -10,13 +7,6 @@ public class SolrCloudUtil {
 	static String zkHost = "127.0.0.1:2181";
 	
 	public static CloudSolrClient getClient() {
-		Properties props = System.getProperties();
-		Enumeration<?> en = props.propertyNames();
-		while(en.hasMoreElements()) {
-			Object key = en.nextElement();
-			Object value = props.get(key);
-			System.out.println(key + "===>" + value);
-		}
 		return new CloudSolrClient(zkHost);
 	}
 
